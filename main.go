d03f35e1ba32bc2f4783d2c7d5c0f00f3735c23c
Peregrine Balas <peregrinereedbalas@gmail.com>
2024-01-06 19:57:27 -0600

    server basics

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Printf("placeholder")
+	s := &http.Server{
+		Addr: ":8080",
+		// Handler:        myHandler,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Fatal(s.ListenAndServe())
 }
